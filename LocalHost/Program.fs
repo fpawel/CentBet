f942bd75f56b0b7914b132245ca9b7b3f9710b4e
fpawel <binf@mail.ru>
2016-03-11 16:09:52 +0300

    add Localhost prj

---
+++ LocalHost/Program.fs
@@ -0,0 +1,6 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.org
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+[<EntryPoint>]
+let main argv = 
+    WebSharper.Warp.RunAndWaitForInput( CentBet.Site.Main )
