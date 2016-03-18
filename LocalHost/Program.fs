c05fe7f85324ee49375a255482c2654daa9d8cbb
fpawel <binf@mail.ru>
2016-03-18 09:08:09 +0300

    add localhost.fsx to gitignore

--- LocalHost/Program.fs
+++ LocalHost/Program.fs
@@ -1,6 +1,4 @@
-﻿// Дополнительные сведения о F# см. на http://fsharp.org
-// Дополнительную справку см. в проекте "Учебник по F#".
-
+﻿
 [<EntryPoint>]
 let main argv = 
     //Json.Serialization.deserialize<Betfair.ApiNG.MarketId> (Json.String "1.123123")
